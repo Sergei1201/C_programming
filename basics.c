842c1a4da5b568892a297990fe1a0f356285e0a2
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-02-21 20:55:42 +0500

    basic input in C

---
+++ basics.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function prototype & forward declaration
+double circle_square(double *radius);
+
+int main()
+{
+    double radius;
+    double square;
+    printf("Enter the radius of a circle: ");
+    scanf("%lf", &radius);
+
+    square = circle_square(&radius);
+
+    printf("The square of the circle is going to be: %lf\n", square);
+
+    return 0;
+}
+
+// Function definition
+double circle_square(double *radius)
+{
+    const double pi = 3.14;
+    return pi * *radius * *radius;
+}
